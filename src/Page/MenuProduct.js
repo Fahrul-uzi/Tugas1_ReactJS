7f8fa02dc4dfb18040d17ee00742de10305d063b
Fahrul-uzi <fahrulf594@gmail.com>
2019-05-01 07:54:11 +0700

    Class Component

---
+++ src/Page/MenuProduct.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+  } from 'react';
+  
+class MenuProduct extends Component {
+render() {
+    return (
+    <div>
+        <p>Ini Menu Product</p>
+    </div>
+    );
+}
+}
+
+export default MenuProduct;
---
+++ src/Page/MenuTentangKami.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+  } from 'react';
+  
+class MenuTentangKami extends Component {
+render() {
+    return (
+    <div>
+        <p>Ini Menu Tentang Kami</p>
+    </div>
+    );
+}
+}
+
+export default MenuTentangKami;
